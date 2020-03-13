928ff77b01a6177a3ead6e29f1e6d5e4fed49ab9
pang-wipada <38246310+pang-wipada@users.noreply.github.com>
2020-03-13 15:50:18 +0700

    update

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h2>Username : {{ username }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username : "Pang Wipada"
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+    <h2>{{ address }}</h2>
+    <UserProfile></UserProfile>
+    <hello-world msg="Hello 1"></hello-world>
+    <hello-world msg="Hello 2"></hello-world>
+    <hello-world></hello-world>
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+import UserProfile from "@/components/UserProfile.vue";
+
+export default {
+  data() {
+    return {
+      address: "Prince of Songkla University, Phuket"
+    };
+  },
+  components: {
+    HelloWorld,
+    UserProfile
+  }
+};
+</script>
+
+<style>
+</style>
